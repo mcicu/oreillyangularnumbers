e3913381ef1ab92c46e99f3c37b5b36a63b5918c
Mihai CICU <T0232181@ad-01.ent-01.adgroup>
2019-11-19 17:15:01 +0200

    custom directives, appUnless structural directive, ngSwitch

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,24 +1,28 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor;
+  @Input() highlightColor;
   @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
   }
 
 
